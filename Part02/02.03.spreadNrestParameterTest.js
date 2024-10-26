c5bdd853424a27b7608914af1c1f98958879e405
jsy8315 <tdhitbtd@gmail.com>
2024-10-26 12:11:22 +0900

    02.04.Spread,rest 파라미터 연습문제 7개_답안

--- Part02/02.03.spreadNrestParameterTest.js
+++ Part02/02.03.spreadNrestParameterTest.js
@@ -6,7 +6,8 @@
 
 // 위 코드의 출력 결과는?
 // 내 정답코드
-// 김, 밥, 1, 2, 3
+// 김, 밥, 1, 2, 3 
+// ['김', '밥', 1, 2, 3 ] 
 
 // 02. spread 문제2
 // var a = [1,2,3];
@@ -18,16 +19,18 @@
 
 // 내 정답코드
 // [1, 2, 3], 'you', 'are', 1 
+//'you'
+
 
 // 03. default 파라미터 문제 1
-// 15 출력, console.log(5 + 10)
+// a자리에 파라미터 하나만 넣음, 9출력
 
 // 04. default 파라미터 문제 2
 // 15 출력
 
 // 05. array를 만들어주는 함수를 제작하고 싶습니다
-// function 어레이(){
-//    return [...arguments]
+// function 어레이(...아몰랑){
+//    return [아몰랑]
 // }
 
 // var newArray = 어레이(1, 2, 3, 4, 5);
@@ -46,25 +49,28 @@
 //     }
 //     console.log(empty.sort())
 // }
+// function 정렬(a){
+//     console.log(...[...a].sort())
+// }
 
 // 정렬('bear');
 
 // 08. 데이터마이닝 기능 만들기
-function 글자세기(a){
+// function 글자세기(a){
 
-    let element = [...a].sort()
-    console.log(element[0])  
-    let result = {}
-    let k = element[0]
-    result.k = 0
-    console.log(result)
-    for(let i = 0; i < element.length; i++){
-        if(element[i - 1] == element[i]){
+//     let element = [...a].sort()
+//     console.log(element[0])  
+//     let result = {}
+//     let k = element[0]
+//     result.k = 0
+//     console.log(result)
+//     for(let i = 0; i < element.length; i++){
+//         if(element[i - 1] == element[i]){
 
-        } else {
+//         } else {
 
-        }
-    }
-}
+//         }
+//     }
+// }
 
-글자세기('aacbbb');
+// 글자세기('aacbbb');
