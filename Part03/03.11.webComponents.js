c508139cead24aedaa96d962883097491b7ef5cd
jsy8315 <tdhitbtd@gmail.com>
2024-11-05 14:50:28 +0900

    docs(JS ES6) : lec 03.11.Web components

---
+++ Part03/03.11.webComponents.js
@@ -0,0 +1,10 @@
+// 03.11.Web Components : 커스텀 HTML 태그 만들기
+
+// class 클래스 extends HTMLElement {
+//     connectedCallback(){ 
+//         // 만든 태그가 HTML에 정착될 때 실행할 코드 적는 곳
+//         this.innerHTML = `<label>이메일인풋이에용<label><input>`
+//     }
+// }
+
+// customElements.define('custom-input', 클래스)
