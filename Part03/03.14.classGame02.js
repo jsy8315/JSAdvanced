3cae16dc7f8de33e62275b1fd78c40bf4dcc6549
jsy8315 <tdhitbtd@gmail.com>
2024-11-07 14:55:47 +0900

    docs(JS ES6) : lec 03.14 \ make function jump and collision

--- Part03/03.14.classGame02.js
+++ Part03/03.14.classGame02.js
@@ -18,9 +18,15 @@ let dino = {
     height : 50,
     draw(){
         ctx.fillStyle = 'green';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        // ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(img02, this.x, this.y);
     }
 }
+
+let img01 = new Image();
+img01.src = 'cactus.png';
+let img02 = new Image();
+img02.src = 'dinosaur.png';
 
 dino.draw();
 
@@ -36,7 +42,8 @@ class Cactus {
     }
     draw(){
         ctx.fillStyle = 'red';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        // ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(img01, this.x, this.y);
     }
 }
 
@@ -45,24 +52,70 @@ class Cactus {
 
 let timer = 0;
 let cactuses = [];
+let 점프중 = false;
+let 점프timer = 0;
+let animation;
 
 function 프레임마다실행할거임(){ // 1초에 60번 코드 실행해야지~
-    requestAnimationFrame(프레임마다실행할거임);
+    animation = requestAnimationFrame(프레임마다실행할거임);
     timer++;
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    if (timer % 60 === 0){
+    if (timer % 240 === 0){
         let cactus01 = new Cactus();
         cactuses.push(cactus01);
     }
 
-    cactuses.forEach(function(a){
+    cactuses.forEach(function(a, i, o){
+        // x좌표가 0미만이면 제거
+        if (a.x < 0) {
+            o.splice(i, 1)
+        }
+
+        충돌진짜하나(dino, a);
+
         a.x--;
         a.draw();
     })
+
+    if (점프중 == true){
+        dino.y--;
+        점프timer++;
+    }
+
+    if(점프timer > 100){
+        점프중 = false;
+        점프timer = 0;
+    }
+
+    if(점프중 == false){
+        if (dino.y < 200){
+            dino.y++;
+        }
+    }
+
+
     
     dino.draw();
 }
 
+// 충돌확인
+function 충돌진짜하나(dino, cactus){
+    let x축차이 = cactus.x - (dino.x + dino.width);
+    let y축차이 = cactus.y - (dino.y + dino.height);
+    if (x축차이 < 0 && y축차이 < 0){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        cancelAnimationFrame(animation);
+    }
+}
+
+
 프레임마다실행할거임();
+
+
+document.addEventListener('keydown', function(e){
+    if(e.code === 'Space'){
+        점프중 = true;
+    }
+})
